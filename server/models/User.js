0da01a52a2d8e16684f3cb14414a1a7c20de3353
SatabdoM <105154633+SatabdoM@users.noreply.github.com>
2024-03-17 13:41:57 +0530

    before

--- server/models/User.js
+++ server/models/User.js
@@ -56,7 +56,5 @@ const userSchema = new mongoose.Schema({
 // Add timestamps for when the document is created and last modified
 { timestamps: true }
 );
-
 // Export the Mongoose model for the user schema, using the name "user"
-
 module.exports = mongoose.model("User", userSchema);
